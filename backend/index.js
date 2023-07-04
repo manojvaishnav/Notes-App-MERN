65057852331c9a7137a11bb75490b1f40d7b0e90
manojvaishnav <manojvaishnav989@gmail.com>
2023-07-04 20:26:24 +0530

    path setup

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const notesRoutes = require("./routes/notesRoutes");
 const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -11,6 +14,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+app.use(morgan("dev"));
 
 // Database Connection
 require("./db/dbConnection");
@@ -23,6 +28,12 @@ const notes = require("./models/notesSchema");
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", notesRoutes);
+
+// Static Files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 // Start the server
 app.listen(port, () => {
